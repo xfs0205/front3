73a6c07a991456a8b6199dd17e868f92b3605ede
xfs0205 <112708866+xfs0205@users.noreply.github.com>
2025-01-19 10:45:17 +0800

    0.11

--- Front/src/views/HomeView.vue
+++ Front/src/views/HomeView.vue
@@ -3,5 +3,16 @@
 </script>
 
 <template>
-
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
 </template>
+
+<style scoped lang="scss">
+
+</style>
+
