24a1a4ef16a603ec31d6584b7a765eaef7bc7d50
xfs0205 <112708866+xfs0205@users.noreply.github.com>
2025-01-20 11:11:31 +0800

    0.17

--- Front/src/components/MainComponents/MainView.vue
+++ Front/src/components/MainComponents/MainView.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import VideoComponent from '@/components/MainComponents/VideoComponent.vue'
+import Module2 from '@/components/MainComponents/Module2.vue';
+import Module3 from '@/components/MainComponents/Module3.vue';
+import Module4 from '@/components/MainComponents/Module4.vue';
 
 </script>
 
@@ -8,6 +11,15 @@ import VideoComponent from '@/components/MainComponents/VideoComponent.vue'
         <div class="module-1">
             <VideoComponent />
         </div>
+    </div>
+    <div class="module-2">
+        <Module2></Module2>
+    </div>
+    <div class="module-3">
+        <Module3></Module3>
+    </div>
+    <div class="module-4">
+        <Module4></Module4>
     </div>
 </template>
 <style scoped>
@@ -23,5 +35,23 @@ import VideoComponent from '@/components/MainComponents/VideoComponent.vue'
     width: 100%;
     height: 900px;
     background-color: transparent;
+}
+
+.module-2{
+    width: 100%;
+    height: 500px;
+    background-color: aqua;
+}
+
+.module-3{
+    width: 100%;
+    height: 500px;
+    background-color: rgb(89, 228, 177);
+}
+
+.module-4{
+    width: 100%;
+    height: 500px;
+    background-color: rgb(255, 252, 71);
 }
 </style>
